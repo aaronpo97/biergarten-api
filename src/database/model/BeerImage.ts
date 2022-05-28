587b8e009ebeb4e57a48105611851484a899dc9a
Aaron William Po <apo2@uwo.ca>
2022-05-28 08:46:11 -0400

    Cloudinary integration and multer update
    Multer no longer uploads to local storage, uses cloudinary instead. Updated multer to 1.4.4-lts.1 to fix CVE-2022-24434.

--- src/database/model/BeerImage.ts
+++ src/database/model/BeerImage.ts
@@ -7,6 +7,7 @@ import {
   JoinColumn,
 } from 'typeorm';
 import Beer from './Beer';
+import User from './User';
 
 @Entity()
 export default class BeerImage extends BaseEntity {
@@ -19,4 +20,8 @@ export default class BeerImage extends BaseEntity {
   @JoinColumn()
   @ManyToOne(() => Beer, (beer) => beer.images)
   beerPost!: Beer;
+
+  @JoinColumn()
+  @ManyToOne(() => User, (user) => user.beerImages)
+  author!: User;
 }
