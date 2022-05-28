47cca37349d7c91945ac22d21a3ca68b585b80e9
Aaron William Po <apo2@uwo.ca>
2022-05-28 03:55:29 -0400

    Added beer_image table to database
    Relation added with beer posts - one beer post has many images

--- src/routes/beerRoutes.ts
+++ src/routes/beerRoutes.ts
@@ -4,11 +4,15 @@ import createNewBeer from '../controllers/beers/create/createNewBeer';
 import deleteBeerById from '../controllers/beers/delete/deleteBeerById';
 import getAllBeers from '../controllers/beers/read/getAllBeers';
 import getBeerById from '../controllers/beers/read/getBeerById';
+import processImageData from '../controllers/beerImages/create/processImageData';
 import updateBeerById from '../controllers/beers/update/updateBeerById';
+
 import checkIfUserIsConfirmed from '../middleware/auth/checkIfUserIsConfirmed';
 import checkTokens from '../middleware/auth/checkTokens';
 import getCurrentUser from '../middleware/auth/getCurrentUser';
+
 import ServerError from '../util/error/ServerError';
+import uploadFile from '../util/imageUpload/uploadFile';
 
 /** Route handler for '/api/beers'. */
 const beerRoutes = express.Router();
@@ -29,6 +33,14 @@ beerRoutes
   .delete(deleteBeerById)
   .all((req, res, next) => {
     res.set('Allow', 'GET, PUT, DELETE');
+    next(new ServerError('Not allowed', 405));
+  });
+
+beerRoutes
+  .route('/:beerId/upload-image')
+  .post(checkTokens, getCurrentUser, uploadFile.array('beer-image'), processImageData)
+  .all((req, res, next) => {
+    res.set('Allow', 'POST');
     next(new ServerError('Not allowed', 405));
   });
 
--- src/util/imageUpload/uploadFile.ts
+++ src/util/imageUpload/uploadFile.ts
@@ -1,16 +1,16 @@
 import multer from 'multer';
+import path from 'path';
 
-
-const storage = multer.diskStorage({
+const localStorage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, `${__dirname}/../../../tmp/images`);
+    cb(null, `${__dirname}/../../../tmp`);
   },
   filename(req, file, cb) {
-    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-    cb(null, `${file.fieldname}-${uniqueSuffix}`);
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 10000000)}`;
+    cb(null, `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`);
   },
 });
 
-const uploadFile = multer({ storage });
+const uploadFile = multer({ storage: localStorage });
 
 export default uploadFile;
