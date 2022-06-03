05bb62ec31fa116ce863ac28967c194d03943c69
Aaron William Po <apo2@uwo.ca>
2022-06-03 12:19:00 -0400

    Fix HTTP method issue in beer comments. Closes #9

--- src/routes/beerCommentRoutes.ts
+++ src/routes/beerCommentRoutes.ts
@@ -7,18 +7,27 @@ import getCommentById from '../controllers/comments/read/getCommentById';
 import editCommentById from '../controllers/comments/update/editCommentById';
 import checkTokens from '../middleware/auth/checkTokens';
 import getCurrentUser from '../middleware/auth/getCurrentUser';
+import ServerError from '../util/error/ServerError';
 
 const commentRoutes = Router({ mergeParams: true });
 
 commentRoutes
   .route('/')
   .get(getAllComments)
-  .post(checkTokens, getCurrentUser, createNewComment);
+  .post(checkTokens, getCurrentUser, createNewComment)
+  .all((req, res, next) => {
+    res.set('Allow', 'GET, POST');
+    next(new ServerError('Not allowed', 405));
+  });
 
 commentRoutes
   .route('/:commentId')
   .get(getCommentById)
   .delete(deleteCommentById)
-  .put(editCommentById);
-
+  .put(editCommentById)
+  .all((req, res, next) => {
+    res.set('Allow', 'GET, DELETE, PUT');
+    next(new ServerError('Not allowed', 405));
+  });
+  
 export default commentRoutes;
